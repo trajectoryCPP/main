02e09cf4b49bfb7659bdc1e4812602a5a091ad59
Justin Dury-Agri <justin.dury.agri@gmail.com>
2020-11-20 11:10:37 -0800

    base file for manipulation

---
+++ helloworld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    {
+        cout << "Enter a random word next." << '\n';
+        string inp;
+        cin >> inp;
+        cout << "You inputed " << inp;
+    }
+    cout << endl;
+}
